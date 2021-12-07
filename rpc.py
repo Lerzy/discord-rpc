73921374a22d2f330e8fcb30f432e0a1c03a5831
Lerzy <Lerzy@users.noreply.github.com>
2021-12-07 20:05:36 +0200

    fix images not working

--- rpc.py
+++ rpc.py
@@ -1,9 +1,9 @@
 from pypresence import Presence
 import time
 
-def RPCRunner(client_id, state, details, large_image, large_text, small_image, small_text, buttons):
+def RPCRunner(client_id, state, details, large_text, large_image, small_text, small_image, buttons):
 
-    kwargs = dict(state=state, details=details, large_image=large_image, large_text=large_text, small_image=small_image, small_text=small_text, buttons=buttons)
+    kwargs = dict(state=state, details=details, large_text=large_text, large_image=large_image, small_text=small_text, small_image=small_image, buttons=buttons)
 
     RPC = Presence(client_id) 
     RPC.connect() 
