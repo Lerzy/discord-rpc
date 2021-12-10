407dbbaec6c66ce9b735de8e77e6f0c05a38d1dc
Lerzy <71385234+Lerzy@users.noreply.github.com>
2021-12-10 15:50:16 +0200

    Add support for party size

--- rpc.py
+++ rpc.py
@@ -1,13 +1,14 @@
 from pypresence import Presence
 import time
 
-def RPCRunner(client_id, state, details, large_text, large_image, small_text, small_image, buttons):
+def RPCRunner(client_id, state, details, large_text, large_image, small_text, small_image, buttons, party_size):
 
-    kwargs = dict(state=state, details=details, large_text=large_text, large_image=large_image, small_text=small_text, small_image=small_image, buttons=buttons)
+    kwargs = dict(state=state, details=details, large_text=large_text, large_image=large_image, small_text=small_text, small_image=small_image, buttons=buttons, party_size=party_size)
 
     RPC = Presence(client_id) 
-    RPC.connect() 
-    print(RPC.update(**{k: v for k, v in kwargs.items() if v != ""}, start=time.time()))  # Set the presence
+    RPC.connect()
+
+    return(RPC.update(**{k: v for k, v in kwargs.items() if v != ""}, start=time.time()))  # Set the presence and return it
 
     while True:
         time.sleep(15)
